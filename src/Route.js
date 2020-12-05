6cf543d69530834a7b7c491a7746eb59c7ca6aad
lsr <imlimr1232@gmail.com>
2020-12-05 14:31:07 +0900

    init

---
+++ src/Route.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const Route = () => {
+  return (
+    <Route>
+      <Switch>
+        <Route exact path="/" component={} />
+      </Switch>
+    </Route>
+  );
+};
+
+export default Route;
---
+++ src/pages/Luggage/Luggage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Luggage = () => {
+  return <div></div>;
+};
+
+export default Luggage;
